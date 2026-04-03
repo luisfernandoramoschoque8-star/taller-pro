70e32f1e7dc714e5b9dac13c951d05562d62220b
Luis Fernando Ramos Choque <luisfernandoramoschoque8@gmail.com>
2026-04-02 21:12:40 -0400

    cors configurado para vercel

--- taller-pro/config/cors.php
+++ taller-pro/config/cors.php
@@ -6,7 +6,7 @@ return [
 	'allowed_origins' => [
 		env('FRONTEND_URL', 'http://localhost:5173'),
 	],
-	'allowed_origins_patterns' => [],
+	'allowed_origins_patterns' => ['https://taller-pro-three.vercel.app'],
 	'allowed_headers' => ['*'],
 	'exposed_headers' => [],
 	'max_age' => 0,
